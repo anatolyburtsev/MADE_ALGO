a2843d2dad8635ee95b398cebe8cdce80fb38028
onotole <aburtsev@ozon.ru>
2019-11-23 21:30:22 -0800

    removed decreasing number of elements in Remove

--- task8_2.cpp
+++ task8_2.cpp
@@ -21,14 +21,12 @@ public:
 
     bool Remove(const std::string &key);
 
-    void Print();
-
     int Hash(const std::string *key) const;
 
 private:
     // value of "a" for hash function
     int HASH_A = 137;
-    double LOAD_FACTOR = 0.74;
+    double LOAD_FACTOR = 0.75;
     int numberOfElements;
 
     struct HashTableNode {
@@ -44,7 +42,6 @@ private:
     };
 
     std::vector<HashTableNode *> *table;
-
 
     int Hash2(const std::string *key) const;
 
@@ -69,20 +66,8 @@ Hashtable::~Hashtable() {
     delete table;
 }
 
-//int Hashtable::Hash(const std::string *key) const {
-//    long A = 2654435769;
-//    int hash = (key * A);
-//    for (char i : *key) {
-//        hash = (hash * HASH_A + i);
-//    }
-//    return hash;
-//}
-
 int Hashtable::Hash(const std::string *key) const {
     int hash = 0;
-//    for (char i : *key) {
-//        hash = (hash * HASH_A + i);
-//    }
     const char *str = key->c_str();
     for (; *str != 0; ++str) {
         hash = (hash * HASH_A + *str);
@@ -92,15 +77,11 @@ int Hashtable::Hash(const std::string *key) const {
 
 
 int Hashtable::Hash2(const std::string *key) const {
-//    return 1;
     int hash = 5;
     const char* str = key->c_str();
     for (; *str != 0; ++str) {
         hash += *str;
     }
-//    for (char i : *key) {
-//        hash += i;
-//    }
     return 2 * hash + 1;
 }
 
@@ -178,7 +159,6 @@ int Hashtable::findElement(int hash, const std::string &key) const {
         if (element->key == key && !element->isDeleted) {
             return position;
         }
-
         position = (position + hash2) % m;
     }
 }
@@ -201,24 +181,6 @@ bool Hashtable::Add(const std::string &key) {
     }
 }
 
-void Hashtable::Print() {
-    std::cout << std::endl;
-    for (int i = 0; i < table->size(); ++i) {
-        if (table->at(i)) {
-            std::cout << table->at(i)->key << ":";
-            if (table->at(i)->isDeleted) {
-                std::cout << "-";
-            } else {
-                std::cout << "+";
-            }
-            std::cout << " ";
-        } else {
-            std::cout << "null" << " ";
-        }
-    }
-    std::cout << std::endl;
-}
-
 void Hashtable::grow() {
     std::vector<HashTableNode *> *old_data = table;
     table = new std::vector<HashTableNode *>(2 * table->size());
@@ -232,6 +194,7 @@ void Hashtable::grow() {
         table->at(index) = node;
     }
 
+    old_data->clear();
     delete old_data;
 }
 
@@ -247,57 +210,30 @@ bool Hashtable::Remove(const std::string &key) {
         return false;
     } else {
         HashTableNode *elem = table->at(index);
-        numberOfElements--;
         elem->isDeleted = true;
         return true;
     }
 }
-#include <fstream>
 
 int main() {
     Hashtable table(8);
     char operation;
     std::string value;
-    std::ifstream inFile;
-    inFile.open("/Users/onotole/CLionProjects/made_algo/input.txt");
-    clock_t begin = clock();
-    while(inFile >> operation >> value) {
-//    while (std::cin >> operation >> value) {
+    while (std::cin >> operation >> value) {
         switch (operation) {
             case '+':
-//                table.Add(value);
                 std::cout << (table.Add(value) ? "OK" : "FAIL") << std::endl;
                 break;
             case '-':
-//                table.Remove(value);
                 std::cout << (table.Remove(value) ? "OK" : "FAIL") << std::endl;
                 break;
             case '?':
-//                table.Has(value);
                 std::cout << (table.Has(value) ? "OK" : "FAIL") << std::endl;
                 break;
             default:
                 assert(false);
         }
-//        table.Print();
     }
-    clock_t end = clock();
-    std::cout << "Elapsed cycles: " << end - begin << std::endl;
-
-//    std::cout << "result: " << table.Add("a") << std::endl;
-//    std::cout << table.Remove("a") << std::endl;
-//    std::cout << "result: " << table.Add("b") << std::endl;
-//    std::cout << "result: " << table.Add("c") << std::endl;
-//    std::cout << "result: " << table.Add("d") << std::endl;
-//    table.Print();
-//    std::cout << table.Has("a") << std::endl;
-//    std::cout << "result: " << table.Add("Pew2") << std::endl;
-//    std::cout << table.Has("at") << std::endl;
-//    std::cout << "result: " << table.Add("ad") << std::endl;
-//    table.Print();
-//    table.Print();
-//    table.Print();
-//    table.Print();
     return 0;
 }
 
