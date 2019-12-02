904441b7cd3406edcb95b6b19f39e9856abe0574
onotole <aburtsev@ozon.ru>
2019-12-01 20:49:24 -0800

    Replace pointer to vector of pointer with vector of object to avoid memory fragmentation and other improvements

--- task8_2.cpp
+++ task8_2.cpp
@@ -31,32 +31,40 @@ public:
     bool Remove(const std::string &key);
 
 private:
-
     // value of "a" for hash function
     int HASH_A = 137;
     double LOAD_FACTOR = 0.75;
     int numberOfElements;
+
     struct HashTableNode {
         int hash;
         std::string key;
         bool isDeleted;
+        bool isDummy;
 
-        HashTableNode(std::string key_, int hash_) {
-            this->key = std::move(key_);
-            this->hash = hash_;
-            this->isDeleted = false;
+        // element was initialized only by default value
+        bool isDummyElement() const { return isDummy; }
+
+        HashTableNode(std::string key_, int hash_) : key(std::move(key_)), hash(hash_), isDeleted(false), isDummy(false) {}
+
+        void update(std::string key_, int hash_) {
+            key = std::move(key_);
+            hash = hash_;
+            isDeleted = false;
+            isDummy = false;
         }
+
+        HashTableNode() : key(""), hash(-1), isDeleted(true), isDummy(true) {}
     };
 
-    std::vector<HashTableNode *> *table;
 
-    int Hash(const std::string *key) const;
+    std::vector<HashTableNode> table;
 
-    int Hash2(const std::string *key) const;
+    int Hash(const std::string &key) const;
+
+    int Hash2(const std::string &key) const;
 
     int findIndexByKeyForAdd(int hash, const std::string &key) const;
-
-    bool hasKey(int hash, const std::string &key) const;
 
     int findElement(int hash, const std::string &key) const;
 
@@ -64,18 +72,17 @@ private:
 };
 
 Hashtable::Hashtable(size_t initial_size) {
-    table = new std::vector<HashTableNode *>(initial_size);
+    table = std::vector<HashTableNode>(initial_size);
     numberOfElements = 0;
 }
 
 Hashtable::~Hashtable() {
-    table->clear();
-    delete table;
+    table.clear();
 }
 
-int Hashtable::Hash(const std::string *key) const {
+int Hashtable::Hash(const std::string &key) const {
     int hash = 0;
-    const char *str = key->c_str();
+    const char *str = key.c_str();
     for (; *str != 0; ++str) {
         hash = (hash * HASH_A + *str);
     }
@@ -83,9 +90,9 @@ int Hashtable::Hash(const std::string *key) const {
 }
 
 
-int Hashtable::Hash2(const std::string *key) const {
+int Hashtable::Hash2(const std::string &key) const {
     int hash = 5;
-    const char* str = key->c_str();
+    const char *str = key.c_str();
     for (; *str != 0; ++str) {
         hash += *str;
     }
@@ -93,77 +100,51 @@ int Hashtable::Hash2(const std::string *key) const {
 }
 
 int Hashtable::findIndexByKeyForAdd(int hash, const std::string &key) const {
-    int m = table->size();
+    int m = table.size();
     int initial_position = ((hash % m) + m) % m;
-    if (table->at(initial_position) == nullptr) {
+    if (table.at(initial_position).isDummyElement()) {
         return initial_position;
     }
-    int hash2 = Hash2(&key) % m;
+    int hash2 = Hash2(key) % m;
     int position = initial_position;
     int position_to_put_new_element = -1;
     while (true) {
-        HashTableNode *element = table->at(position);
+        HashTableNode element = table.at(position);
         // found empty place for new element
-        if (element == nullptr) {
+        if (element.isDummyElement()) {
             if (position_to_put_new_element < 0) position_to_put_new_element = position;
             break;
         }
         // found place for new element with deleted previous one
-        if (element->isDeleted && position_to_put_new_element < 0) position_to_put_new_element = position;
+        if (element.isDeleted && position_to_put_new_element < 0) position_to_put_new_element = position;
 
         // found duplicate, fatal for Add
-        if (element->key == key && !element->isDeleted) return -1;
+        if (element.key == key && !element.isDeleted) return -1;
 
         position = (position + hash2) % m;
     }
     return position_to_put_new_element;
 }
 
-bool Hashtable::hasKey(int hash, const std::string &key) const {
-    int m = table->size();
-    int initial_position = ((hash % m) + m) % m;
-    if (table->at(initial_position) == nullptr) {
-        return false;
-    }
-    int hash2 = Hash2(&key) % m;
-    int position = initial_position;
-    while (true) {
-        HashTableNode *element = table->at(position);
-        if (element == nullptr) {
-            return false;
-        }
-
-        if (element->key == key && element->isDeleted) {
-            return false;
-        }
-
-        if (element->key == key && !element->isDeleted) {
-            return true;
-        }
-
-        position = (position + hash2) % m;
-    }
-}
-
 int Hashtable::findElement(int hash, const std::string &key) const {
-    int m = table->size();
+    int m = table.size();
     int initial_position = ((hash % m) + m) % m;
-    if (table->at(initial_position) == nullptr) {
+    if (table.at(initial_position).isDummyElement()) {
         return -1;
     }
-    int hash2 = Hash2(&key) % m;
+    int hash2 = Hash2(key) % m;
     int position = initial_position;
     while (true) {
-        HashTableNode *element = table->at(position);
-        if (element == nullptr) {
+        HashTableNode element = table.at(position);
+        if (element.isDummyElement()) {
             return -1;
         }
 
-        if (element->key == key && element->isDeleted) {
+        if (element.key == key && element.isDeleted) {
             return -1;
         }
 
-        if (element->key == key && !element->isDeleted) {
+        if (element.key == key && !element.isDeleted) {
             return position;
         }
         position = (position + hash2) % m;
@@ -172,54 +153,52 @@ int Hashtable::findElement(int hash, const std::string &key) const {
 
 
 bool Hashtable::Add(const std::string &key) {
-    int hash = Hash(&key);
-    auto *node = new HashTableNode(key, hash);
+    int hash = Hash(key);
     int index = findIndexByKeyForAdd(hash, key);
     if (index < 0) {
         return false;
-    } else {
-        table->at(index) = node;
-        numberOfElements++;
-        double load_factor = double(numberOfElements) / table->capacity();
-        if (load_factor >= LOAD_FACTOR) {
-            grow();
-        }
-        return true;
     }
+
+    table.at(index).update(key, hash);
+    numberOfElements++;
+    double load_factor = double(numberOfElements) / table.capacity();
+    if (load_factor >= LOAD_FACTOR) {
+        grow();
+    }
+    return true;
 }
 
 void Hashtable::grow() {
-    std::vector<HashTableNode *> *old_data = table;
-    table = new std::vector<HashTableNode *>(2 * table->size());
-    for (HashTableNode *node: *old_data) {
-        if (!node) continue;
-        if (node->isDeleted) continue;
-        int index = findIndexByKeyForAdd(node->hash, node->key);
+    std::vector<HashTableNode> old_data = table;
+    table = std::vector<HashTableNode>(2 * table.size());
+    for (const HashTableNode& node: old_data) {
+        if (node.isDummyElement()) continue;
+        if (node.isDeleted) continue;
+        int index = findIndexByKeyForAdd(node.hash, node.key);
         if (index < 0) {
             throw std::runtime_error("grow command failed with negative index");
         }
-        table->at(index) = node;
+        table.at(index).update(node.key, node.hash);
     }
-
-    old_data->clear();
-    delete old_data;
+    // easy way to delete vector from memory based on
+    // https://stackoverflow.com/questions/10464992/c-delete-vector-objects-free-memory
+    std::vector<HashTableNode>().swap(old_data);
 }
 
 bool Hashtable::Has(const std::string &key) const {
-    int hash = Hash(&key);
-    return hasKey(hash, key);
+    int hash = Hash(key);
+    return findElement(hash, key) >= 0;
 }
 
 bool Hashtable::Remove(const std::string &key) {
-    int hash = Hash(&key);
+    int hash = Hash(key);
     int index = findElement(hash, key);
     if (index < 0) {
         return false;
-    } else {
-        HashTableNode *elem = table->at(index);
-        elem->isDeleted = true;
-        return true;
     }
+    table.at(index).isDeleted = true;
+    return true;
+
 }
 
 int main() {
